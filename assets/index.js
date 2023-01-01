8d74f438ff4ebc404639e5ed2360bf2d4637e9aa
daniel <michigamidaniel77@gmail.com>
2022-12-31 22:26:54 -0400

    add configs

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+const btnDarkAndLightMode = document.querySelector('.switch button')
+const themeDarkAndMode = document.querySelector('html')
+
+btnDarkAndLightMode.addEventListener('click', () => {
+    themeDarkAndMode.classList.toggle('light')
+}) 
+
+
+
